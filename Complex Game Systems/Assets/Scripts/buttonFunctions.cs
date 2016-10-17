f9bcacd1af1ce664b8a60a567effbd8890179288
Brock Barlow <brock.barlow@students.aie.edu.au>
2016-10-17 17:27:08 -0500

    created button script and game over scene

--- Complex Game Systems/Assets/Scripts/buttonFunctions.cs
+++ Complex Game Systems/Assets/Scripts/buttonFunctions.cs
@@ -1,10 +1,11 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class buttonFunctions : MonoBehaviour {
 
 	public void restart()
     {
-
+        SceneManager.LoadScene("BrockScene");
     }
 }
