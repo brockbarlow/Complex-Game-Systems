73dbd18041ac2ba013869a4a9ec8fc116b6febfc
Austin Morrell <austinlm19@gmail.com>
2016-10-18 14:00:29 -0500

    Thumbs up

---
+++ Complex Game Systems/Assets/Scripts/Despawn.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Despawn : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(Fade());
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if (GetComponent<Image>().color.a <= 0)
+        {
+            Destroy(gameObject);
+        }
+	}
+
+    public IEnumerator Fade()
+    {
+        while (GetComponent<Image>().color.a > 0)
+        {
+            Color color = GetComponent<Image>().color;
+            color.a -= 0.1f;
+            GetComponent<Image>().color = color;
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+}
