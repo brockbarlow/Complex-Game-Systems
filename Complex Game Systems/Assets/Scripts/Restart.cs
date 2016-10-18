1a72c866aab28567b6aa4a0afe09bc57c68fc4ae
Austin Morrell <austinlm19@gmail.com>
2016-10-18 10:56:15 -0500

    Slight change to input

--- Complex Game Systems/Assets/Scripts/Restart.cs
+++ Complex Game Systems/Assets/Scripts/Restart.cs
@@ -7,7 +7,7 @@ public class Restart : MonoBehaviour {
     void Update()
     {
         // Will return the user to the main menu after pressing B.
-        if (Input.GetKeyDown("r") || Input.GetButtonDown("R"))
+        if (Input.GetKeyDown("r") || Input.GetKeyDown("R"))
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
