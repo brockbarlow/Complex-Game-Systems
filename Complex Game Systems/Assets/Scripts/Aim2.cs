4cd8409495dd4c1a8f660b16f756afa119813a72
Donte Wilson <JittNA@aol.com>
2016-10-18 11:01:42 -0500

    Edits
    Working on new Aim script

---
+++ Complex Game Systems/Assets/Scripts/Aim2.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System;
+
+public class Aim2 : MonoBehaviour, IPointerClickHandler, IPointerDownHandler, IPointerUpHandler {
+
+    GameObject Cube;
+   
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+
+    }
+    public void OnPointerClick(PointerEventData eventData)
+    {
+       
+       
+        Debug.Log("Gotteeem");
+       
+    }
+
+   
+
+    
+}
